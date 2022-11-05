04418e3dc08ebb47e58021735e82b70727cf16af
DiegoYukioKoyama <yukioko.jp@gmail.com>
2022-11-05 16:35:20 -0300

    inicio js tela 3.2

--- projeto6-buzzquizz/script.js
+++ projeto6-buzzquizz/script.js
@@ -26,3 +26,81 @@ function quizzesNaTela(valor) {
 }
 
 pegarQuizzesDaAPI();
+
+
+// Parte Diego Koyama
+let tituloQuizz = document.querySelector(".titulo").value;
+let urlQuizz = document.querySelector(".url").value;
+let nPerguntasQuizz = document.querySelector(".nQuizz").value;
+let niveisQuizz = document.querySelector(".niveis").value;
+const containerCriacao1 = document.querySelector(".container-criacao1");
+const containerCriacao2 = document.querySelector(".container-criacao2");
+let respostaVUrl;
+
+// Tela 3.2 - Perguntas do quizz
+nPerguntas();
+
+function nPerguntas(){
+    const pergunta = document.querySelector(".info-Pergunta");
+    pergunta.innerHTML = "";    
+    for(i = 0; i < nPerguntasQuizz; i++){
+        let cont = i+1;
+        if(cont===1){
+            pergunta.innerHTML += `
+            <li class="entrada-infop1 ${cont}">
+        
+                <p>Pergunta ${cont}</p>
+                <input type="text" class="texto-pergunta container-input" placeholder="Texto da pergunta">
+                <input type="text" class="cor-fundo container-input" placeholder="Cor de fundo da pergunta">
+                <p>Resposta correta</p>
+                <input type="text" class="resposa-correta container-input" placeholder="Resposta correta">
+                <input type="text" class="url-img container-input" placeholder="URL da imagem">
+                <p>Respostas incorretas</p>
+                <input type="text" class="resposa-incorreta1 container-input" placeholder="Resposta incorreta 1">
+                <input type="text" class="url-img-incorreta1 container-input" placeholder="URL da imagem 1">
+                <input type="text" class="resposa-incorreta2 container-input" placeholder="Resposta incorreta 2">
+                <input type="text" class="url-img-incorreta2 container-input" placeholder="URL da imagem 2">
+                <input type="text" class="resposa-incorreta3 container-input" placeholder="Resposta incorreta 3">
+                <input type="text" class="url-img-incorreta3 container-input" placeholder="URL da imagem 3">
+
+            </li>`
+        
+            pergunta.innerHTML += `
+            <li class="entrada-info-reduzida esconder">
+            <p>Pergunta ${cont}</p>
+            <bottom class="expandir"><ion-icon name="create-outline"></ion-icon></bottom>
+            </li>
+            `
+
+        }
+        else{
+            pergunta.innerHTML += `
+            <li class="entrada-infop1 esconder">
+        
+                <p>Pergunta ${cont}</p>
+                <input type="text" class="texto-pergunta container-input" placeholder="Texto da pergunta">
+                <input type="text" class="cor-fundo container-input" placeholder="Cor de fundo da pergunta">
+                <p>Resposta correta</p>
+                <input type="text" class="resposa-correta container-input" placeholder="Resposta correta">
+                <input type="text" class="url-img container-input" placeholder="URL da imagem">
+                <p>Respostas incorretas</p>
+                <input type="text" class="resposa-incorreta1 container-input" placeholder="Resposta incorreta 1">
+                <input type="text" class="url-img-incorreta1 container-input" placeholder="URL da imagem 1">
+                <input type="text" class="resposa-incorreta2 container-input" placeholder="Resposta incorreta 2">
+                <input type="text" class="url-img-incorreta2 container-input" placeholder="URL da imagem 2">
+                <input type="text" class="resposa-incorreta3 container-input" placeholder="Resposta incorreta 3">
+                <input type="text" class="url-img-incorreta3 container-input" placeholder="URL da imagem 3">
+
+            </li>`
+
+            pergunta.innerHTML += `
+            <li class="entrada-info-reduzida">
+            <p>Pergunta ${cont}</p>
+            <bottom class="expandir"><ion-icon name="create-outline"></ion-icon></bottom>
+            </li>
+            `
+        } 
+        
+    }
+
+}
